5ccec2f5f6b54c56b8f352cfba67bf86a782abb8
paulm29 <paul.miscellaneous@gmail.com>
2023-06-04 12:35:32 +1000

    quote list

---
+++ src/main/webapp/src/app/quote/quote-list/quote-list.component.spec.ts
@@ -0,0 +1,34 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { QuoteListComponent } from './quote-list.component';
+
+describe('QuoteListComponent', () => {
+  let component: QuoteListComponent;
+  let fixture: ComponentFixture<QuoteListComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [QuoteListComponent],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuoteListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/main/webapp/src/app/quote/quote-list/quote-list.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { QuoteListDataSource, QuoteListItem } from './quote-list-datasource';
+
+@Component({
+  selector: 'app-quote-list',
+  templateUrl: './quote-list.component.html',
+  styleUrls: ['./quote-list.component.css']
+})
+export class QuoteListComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<QuoteListItem>;
+  dataSource: QuoteListDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new QuoteListDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
